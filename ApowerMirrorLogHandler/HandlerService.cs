6f65dcd6d57a0ff24f78b6b5e465e173e69503a6
SARezaHashemi <samhashemi87@gmail.com>
2024-10-11 20:55:57 +0330

    Initial commit

---
+++ ApowerMirrorLogHandler/HandlerService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ApowerMirrorLogHandler
+{
+    public partial class HandlerService : ServiceBase
+    {
+        public HandlerService()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle(object sender, ElapsedEventArgs e)
+        {
+            Log($"Checking folder started on {DateTime.Now.ToShortTimeString()}");
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Apowersoft", "ApowerMirror", "log");
+            if (Directory.Exists(path))
+            {
+                FileInfo[] fileInfos = new DirectoryInfo(path).GetFiles();
+                long size = fileInfos.Select(fi => fi.Length).Sum() / (1024 * 1024); // Get Size In MB
+                if(size > 500)
+                {
+                    Directory.Delete(path);
+                    Log($"Log folder checked; All logs file deleted; the total size was {size}");
+                }
+                else
+                {
+                    Log($"Log folder checked; No need to delete; the total size is {size}");
+                }
+            }
+            else
+            {
+                Log("log folder does not exist");
+            }
+        }
+
+        private void Log(string logMessage)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(logMessage);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(logMessage);
+                }
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer()
+            {
+                Interval = 300_000, //300 seconds = five minutes
+                AutoReset = true
+            };
+            timer.Elapsed += new ElapsedEventHandler(Handle);
+            timer.Enabled = true;
+            Log($"Service started on {DateTime.Now.ToString()}");
+        }
+
+        protected override void OnStop()
+        {
+            Log($"Service stoped on {DateTime.Now.ToString()}");
+        }
+    }
+}
---
+++ ApowerMirrorLogHandler/ProjectInstaller.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ApowerMirrorLogHandler
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AMLHandlerServiceProccessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.AMLHandlerServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // AMLHandlerServiceProccessInstaller
+            // 
+            this.AMLHandlerServiceProccessInstaller.Password = null;
+            this.AMLHandlerServiceProccessInstaller.Username = null;
+            // 
+            // AMLHandlerServiceInstaller
+            // 
+            this.AMLHandlerServiceInstaller.ServiceName = "ApowerMirrorLogHandlerService";
+            this.AMLHandlerServiceInstaller.DisplayName = "AMLLogDeletor";
+            this.AMLHandlerServiceInstaller.Description = "This service deletes ApowerMirror logs when its size reaches 500MB";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.AMLHandlerServiceProccessInstaller,
+            this.AMLHandlerServiceInstaller});
+            
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller AMLHandlerServiceProccessInstaller;
+        private System.ServiceProcess.ServiceInstaller AMLHandlerServiceInstaller;
+    }
+}
